f7b4a15c75cccea8dde5c46d5b46fadcfb8ccc0c
John Puddephatt <john@jdp.org.uk>
2023-08-18 13:03:19 +0100

    fix?

--- resources/views/templates/page.blade.php
+++ resources/views/templates/page.blade.php
@@ -4,7 +4,6 @@
     @foreach ($page->content as $layout)
         @include('flexible.' . $layout->name(), [
             'layout' => $layout,
-            'class' => 'xl:max-w-7xl 2xl:max-w-8xl text-left mx-auto',
         ])
     @endforeach
 @endsection
