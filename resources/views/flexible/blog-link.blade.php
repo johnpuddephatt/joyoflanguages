51ce9ee9a3ce814707748229d79b56e9e256be5f
John Puddephatt <john@jdp.org.uk>
2023-08-30 20:48:19 +0100

    blog card

--- resources/views/flexible/blog-link.blade.php
+++ resources/views/flexible/blog-link.blade.php
@@ -1,6 +1,6 @@
 @if ($layout->post)
     <div class="{{ $class ?? 'mx-auto max-w-5xl' }} container py-12">
-        <div class="relative flex flex-col items-center bg-beige lg:ml-16 lg:flex-row lg:py-6">
+        <div class="relative flex flex-col bg-beige lg:ml-16 lg:flex-row lg:items-center lg:py-6">
             <x-library-image conversion="3x2" class="mx-auto w-full lg:-ml-16 lg:max-w-xs" :image="$layout->post->image" />
 
             <div class="relative px-4 py-8 lg:px-8">
