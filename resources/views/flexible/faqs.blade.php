9dc55bba0f415d645703fc0d4037a4145dabab84
John Puddephatt <john@jdp.org.uk>
2023-08-30 23:08:18 +0100

    scrollIntoView faqs

--- resources/views/flexible/faqs.blade.php
+++ resources/views/flexible/faqs.blade.php
@@ -7,7 +7,8 @@
             @foreach ($layout->faqs as $faq)
                 <details x-transition x-show="{{ $loop->index }} < showing"
                     class="border-t border-light-teal marker:text-light-teal" :open="current == {{ $loop->index }}">
-                    <summary @click.prevent="current = (current == {{ $loop->index }}) ? null : {{ $loop->index }}"
+                    <summary
+                        @click.prevent="current = (current == {{ $loop->index }}) ? null : {{ $loop->index }}; $el.scrollIntoView({ behavior: 'smooth', block: 'start' })"
                         class="cursor-pointer px-2 py-4 text-xl font-semibold transition hover:bg-light-teal hover:bg-opacity-10">
                         {{ $faq->question }}</summary>
                     <div class="prose pb-12 pl-7 pr-4 pt-4">@markdown($faq->answer)</div>
