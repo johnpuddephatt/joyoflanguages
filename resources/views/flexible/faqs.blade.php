76b259af08cc53e24239782c21de2711056c2a26
John Puddephatt <john@jdp.org.uk>
2023-08-30 23:15:11 +0100

    scrollintoview timeout

--- resources/views/flexible/faqs.blade.php
+++ resources/views/flexible/faqs.blade.php
@@ -8,7 +8,7 @@
                 <details x-transition x-show="{{ $loop->index }} < showing"
                     class="border-t border-light-teal marker:text-light-teal" :open="current == {{ $loop->index }}">
                     <summary
-                        @click.prevent="current = (current == {{ $loop->index }}) ? null : {{ $loop->index }}; setTimeout(()=> $el.scrollIntoView({ behavior: 'smooth', block: 'start' }), 1000)"
+                        @click.prevent="current = (current == {{ $loop->index }}) ? null : {{ $loop->index }}; setTimeout(()=> $el.scrollIntoView({ behavior: 'smooth', block: 'start' }), 200)"
                         class="cursor-pointer scroll-mt-24 px-2 py-4 text-xl font-semibold transition hover:bg-light-teal hover:bg-opacity-10">
                         {{ $faq->question }}</summary>
                     <div class="prose pb-12 pl-7 pr-4 pt-4">@markdown($faq->answer)</div>
