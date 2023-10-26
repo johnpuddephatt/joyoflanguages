69a575634a797b57223870e249e4343a3759ece2
John Puddephatt <john@jdp.org.uk>
2023-10-26 15:48:57 +0530

    padding

--- resources/views/flexible/timetable.blade.php
+++ resources/views/flexible/timetable.blade.php
@@ -14,7 +14,7 @@
             @endif
             <img class="mx-auto w-full max-w-md lg:w-2/5" src="{{ Storage::url($layout->image) }}" />
 
-            <div class="{{ $layout->reverse ? 'lg:order-first' : null }} relative z-10 flex-1 lg:py-8">
+            <div class="{{ $layout->reverse ? 'lg:order-first' : null }} relative z-10 flex-1 pt-4 2xl:py-8">
                 <h2 class="type-lg max-w-xl">{!! $layout->title !!}</h2>
                 @if ($layout->subtitle)
                     <div class="mt-4 text-xl font-bold lg:text-2xl">{{ $layout->subtitle }}</div>
