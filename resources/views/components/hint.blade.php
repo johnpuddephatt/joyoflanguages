05003e20922fab86f71424248281c86b32e1db91
John Puddephatt <john@jdp.org.uk>
2023-10-27 15:48:45 +0530

    hint w

--- resources/views/components/hint.blade.php
+++ resources/views/components/hint.blade.php
@@ -1,4 +1,4 @@
- <div class="container mx-auto max-w-3xl">
+ <div class="container mx-auto max-w-4xl">
      <div
          class="prose prose-lg relative mt-16 max-w-none rounded-3xl bg-light-teal bg-opacity-20 px-8 pb-4 lg:px-12 lg:pt-8 2xl:px-16 2xl:pb-8 2xl:pt-12">
          {!! $slot !!}
