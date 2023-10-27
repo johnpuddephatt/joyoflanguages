0c0bee8450af5e14b07f23d936fde1a84d84e86b
John Puddephatt <john@jdp.org.uk>
2023-10-27 16:14:04 +0530

    sizing

--- resources/views/flexible/jump-cta.blade.php
+++ resources/views/flexible/jump-cta.blade.php
@@ -1,4 +1,4 @@
-<div class="{{ $layout->background_colour ?? null }} border-b-[3px] pt-16 text-center lg:border-b-4">
+<div class="{{ $layout->background_colour ?? null }} border-b-[3px] pt-8 text-center lg:border-b-4 2xl:pt-16">
     @if ($layout->title)
         <h3 class="type-md !mb-0">{{ $layout->title }}</h2>
     @endif
