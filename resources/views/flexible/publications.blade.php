0df7efcacf5398c4b85c646372d327e42d97e0c4
John Puddephatt <john@jdp.org.uk>
2023-10-28 15:41:08 +0530

    sectioned

--- resources/views/flexible/publications.blade.php
+++ resources/views/flexible/publications.blade.php
@@ -1,4 +1,4 @@
-    <div class="{{ $class ?? 'mx-auto max-w-5xl ' }} container relative py-8 pb-48 lg:pb-16">
+    <div class="{{ $class ?? 'mx-auto max-w-5xl ' }} container relative py-8 pb-24 lg:pb-16">
 
         @foreach ($layout->publications as $publication)
             <div
@@ -20,7 +20,7 @@
             </div>
         @endforeach
         <svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="651.22" height="715.47"
-            class="absolute bottom-0 right-0 -z-10 h-auto w-96" viewBox="0 0 567 715.5">
+            class="absolute -bottom-24 right-0 -z-10 h-auto w-56 lg:bottom-0 lg:w-96" viewBox="0 0 567 715.5">
             <path fill="#f4af6b"
                 d="M24.3 350.9c1.2-5.6 2.2-11.3 3.2-17C46.6 223.8 300.9 51.2 467.9 150.8c97.8 58.4 170.8 100.1 182.8 249.1 5.7 70.3-31.6 233.9-211.7 292.5 0 0-239.7 95.6-379.1-103.4C-1.4 501.7 17 385.2 24.3 350.9z" />
             <path fill="#ffd703"
