8ab984ed968ebe78bb64f4a8c71c69df173d65b1
John Puddephatt <john@jdp.org.uk>
2023-10-28 15:22:21 +0530

    txt w bullets

--- resources/views/flexible/text-with-bullets.blade.php
+++ resources/views/flexible/text-with-bullets.blade.php
@@ -1,7 +1,7 @@
 <div class="relative bg-beige bg-opacity-30">
     <div class="{{ $class ?? 'mx-auto max-w-7xl' }} container gap-x-24 py-12 md:grid md:grid-cols-2 lg:py-32">
         <div class="">
-            <h2 class="mb-8 text-4xl font-bold">{!! $layout->title !!}</h2>
+            <h2 class="text-4xl font-bold">{!! $layout->title !!}</h2>
             <div class="prose-standout prose max-w-lg">
                 @markdown($layout->description)
             </div>
