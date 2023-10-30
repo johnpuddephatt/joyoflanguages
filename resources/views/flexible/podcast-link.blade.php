b8caf41e6674ac9d275298cb3b5a3aecf06083ee
John Puddephatt <john@jdp.org.uk>
2023-10-30 14:16:05 +0530

    podcast card

--- resources/views/flexible/podcast-link.blade.php
+++ resources/views/flexible/podcast-link.blade.php
@@ -1,12 +1,18 @@
 @if ($layout->podcast)
-    <div class="{{ $class ?? 'mx-auto max-w-5xl' }} py-12">
-        <div class="relative ml-16 flex flex-row items-center bg-yellow py-6">
-            <x-library-image conversion="3x2" class="mx-auto mb-4 max-w-2xl" :image="$layout->podcast->image" />
+    <div class="{{ $class ?? ' mx-auto max-w-4xl' }} container py-12">
 
-            <div class="container relative pl-16">
-                <h3 class="type-sm max-w-2xl"> {{ $layout->podcast->title }}</h3>
+        <div class="relative flex flex-row items-center gap-6 rounded-l-3xl bg-beige bg-opacity-30">
+            <x-image-mask class="hidden h-auto w-24 rounded-3xl bg-yellow p-4 lg:block lg:w-56">
+                <x-library-image conversion="square" class="relative block h-auto w-full" :image="\App\Models\Page::where('template', 'App\Nova\Templates\PodcastsPageTemplate')
+                    ->where('language_id', $page->language?->id)
+                    ->first()->image" />
+            </x-image-mask>
+            <div class="relative">
+                <p class="bold-text-light-teal type-sm !mb-0.5 font-bold text-teal">Learn Italian
+                    <strong>Podcast</strong>
+                <h3 class="type-sm mb-4.5 max-w-2xl"> {{ $layout->podcast->title }}</h3>
 
-                <x-button-link href="{{ $layout->podcast->url }}" class="mx-auto shadow-white">
+                <x-button-link href="{{ $layout->podcast->url }}" class="mx-auto shadow-yellow">
                     Listen now</x-button-link>
             </div>
         </div>
