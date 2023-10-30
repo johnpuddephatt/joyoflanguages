2543532405c4de5e4170456f17b803a78dd8a3f7
John Puddephatt <john@jdp.org.uk>
2023-10-30 15:31:13 +0530

    podcast icon

--- resources/views/flexible/latest-podcasts.blade.php
+++ resources/views/flexible/latest-podcasts.blade.php
@@ -6,7 +6,7 @@
 
             <div class="space-y-2">
                 @foreach ($layout->podcasts as $podcast)
-                    <div class="relative flex max-w-lg justify-between gap-2 border-b border-light-teal pb-4 pt-2">
+                    <div class="relative flex max-w-lg justify-between gap-3 border-b border-light-teal pb-4 pt-2">
                         <x-image-mask
                             class="mb-auto block h-auto w-[5rem] flex-none rounded-xl bg-yellow p-1.5 lg:w-[5.5rem]">
                             <x-library-image conversion="square" class="relative block h-auto w-full" :image="\App\Models\Page::where('template', 'App\Nova\Templates\PodcastsPageTemplate')
