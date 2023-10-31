129b45f7b144699e761ca60bc249c2007175a83f
John Puddephatt <john@jdp.org.uk>
2023-10-31 14:06:02 +0530

    debug

--- resources/views/flexible/team.blade.php
+++ resources/views/flexible/team.blade.php
@@ -7,15 +7,15 @@
                     @mouseLeave="$refs.video.pause(); $refs.video.currentTime = 0; playing= false"
                     x-data="{ playing: false }">
                     <x-library-image x-transition.opacity x-show="!playing" :image="$member->photo" conversion="3x2"
-                        class="duration-250 mx-auto block w-full transition" />
+                        class="mx-auto block w-full" />
                     @if ($member->video)
                         <video x-ref="video" @ended="$refs.video.currentTime = 0; playing= false" muted
-                            class="absolute inset-0 z-10 h-full w-full" x-show="playing"
-                            x-transition:enter.duration.500ms.delay.250ms x-transition:leave.duration.1000ms>
+                            class="absolute inset-0 z-10 h-full w-full" x-show="playing">
                             <source src="{{ Storage::url($member->video->mp4) }}" type="video/mp4">
                             <source src="{{ Storage::url($member->video->webm) }}" type="video/webm">
 
                         </video>
+                        x-transition:enter.duration.500ms.delay.250ms x-transition:leave.duration.1000ms
                     @endif
                 </div>
                 <h3 class="mt-6 text-3xl font-bold text-blue">{{ $member->name }}</h3>
