0a7e34093374121919901dd48f88868463d0b6c7
John Puddephatt <john@jdp.org.uk>
2023-10-31 14:01:01 +0530

    duration

--- resources/views/flexible/team.blade.php
+++ resources/views/flexible/team.blade.php
@@ -3,14 +3,15 @@
 
         @foreach ($layout->team as $member)
             <div class="block">
-                <div class="relative" @mouseEnter="$refs.video.play(); playing= true"
+                <div class="relative bg-black" @mouseEnter="$refs.video.play(); playing= true"
                     @mouseLeave="$refs.video.pause(); $refs.video.currentTime = 0; playing= false"
                     x-data="{ playing: false }">
-                    <x-library-image :image="$member->photo" conversion="3x2" class="mx-auto block w-full" />
+                    <x-library-image :class="{'opacity-0' : !playing }" :image="$member->photo" conversion="3x2"
+                        class="duration-250 mx-auto block w-full transition" />
                     @if ($member->video)
                         <video x-ref="video" @ended="$refs.video.currentTime = 0; playing= false" muted
                             class="absolute inset-0 z-10 h-full w-full" x-show="playing"
-                            x-transition:enter.duration.500ms x-transition:leave.duration.1000ms>
+                            x-transition:enter.duration.500ms.delay.250ms x-transition:leave.duration.1000ms>
                             <source src="{{ Storage::url($member->video->mp4) }}" type="video/mp4">
                             <source src="{{ Storage::url($member->video->webm) }}" type="video/webm">
 
