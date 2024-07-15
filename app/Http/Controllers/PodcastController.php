db4e2ae1cc6390d5141576129a535e55c039f872
John Puddephatt <john@jdp.org.uk>
2024-07-15 19:45:32 +0100

    cache podcast

--- app/Http/Controllers/PodcastController.php
+++ app/Http/Controllers/PodcastController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Podcast;
 use App\Models\Language;
+use Illuminate\Support\Facades\Cache;
 
 class PodcastController extends Controller
 {
@@ -15,11 +16,14 @@ class PodcastController extends Controller
 
     public static function audio(Language $language = null, Podcast $podcast)
     {
-        $string = file_get_contents($podcast->file);
-        header("Content-Type: audio/mpeg");
-        header("Content-length: " . strlen($string));
-        header("Connection: keep-alive");
-        header("Accept-Ranges: bytes ");
-        echo $string;
+
+        return Cache::remember('users', 600, function () use ($podcast) {
+            $string = file_get_contents($podcast->file);
+            header("Content-Type: audio/mpeg");
+            header("Content-length: " . strlen($string));
+            header("Connection: keep-alive");
+            header("Accept-Ranges: bytes ");
+            echo $string;
+        });
     }
 }
