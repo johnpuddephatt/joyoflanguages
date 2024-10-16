075fabbbd4d3ecbcdd979408b0f2c3f6407dafa2
John Puddephatt <john@jdp.org.uk>
2024-10-16 19:38:26 +0100

    don't error when youtube block content is missing

--- resources/views/blocks/youtube.blade.php
+++ resources/views/blocks/youtube.blade.php
@@ -3,6 +3,8 @@
     <figure class="{{ $class }}">
         <iframe class="block aspect-video w-full" src="{{ str_replace('watch', 'embed', $attrs['src']) }}" frameborder="0"
             allow="autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        @include('blocks.text', ['content' => $content])
+        @if (isset($content))
+            @include('blocks.text', ['content' => $content])
+        @endif
     </figure>
 @endif
