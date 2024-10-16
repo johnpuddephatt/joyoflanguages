35dd8576ae0c237cb30533f1472f3016e9d69851
John Puddephatt <john@jdp.org.uk>
2024-10-16 21:07:43 +0100

    remove class output on youtube

--- resources/views/blocks/youtube.blade.php
+++ resources/views/blocks/youtube.blade.php
@@ -1,5 +1,4 @@
 @if (isset($attrs['src']))
-    {{ $class }}
     <figure class="{{ $class }}">
         <iframe class="block aspect-video w-full" src="{{ str_replace('watch', 'embed', $attrs['src']) }}" frameborder="0"
             allow="autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
