58b44daace6f7cbdcbf493aa1d0ee26622afc3c4
John Puddephatt <john@jdp.org.uk>
2024-10-21 11:00:16 +0100

    fix youtube block

--- resources/views/blocks/youtube.blade.php
+++ resources/views/blocks/youtube.blade.php
@@ -1,7 +1,9 @@
 @if (isset($attrs['src']))
     <figure class="{{ $class }}">
-        <iframe class="block aspect-video w-full" src="{{ str_replace('watch', 'embed', $attrs['src']) }}" frameborder="0"
+        <iframe class="block aspect-video w-full"
+            src="{{ Str::of($attrs['src'])->replace('watch', 'embed')->replace('?v=', '/') }}" frameborder="0"
             allow="autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
         @if (isset($content))
             @include('blocks.text', ['content' => $content])
         @endif
