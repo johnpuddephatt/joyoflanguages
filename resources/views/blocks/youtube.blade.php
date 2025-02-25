04abf3cb0aac9407fcb4ad50303de8ee01f3bd6a
John Puddephatt <john@jdp.org.uk>
2025-02-25 14:23:14 +0000

    update youtube regex

--- resources/views/blocks/youtube.blade.php
+++ resources/views/blocks/youtube.blade.php
@@ -1,11 +1,21 @@
 @if (isset($attrs['src']))
-    <figure class="{{ $class }}">
-        <iframe class="block aspect-video w-full"
-            src="{{ Str::of($attrs['src'])->replace('watch', 'embed')->replace('?v=', '/') }}" frameborder="0"
-            allow="autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
-        @if (isset($content))
-            @include('blocks.text', ['content' => $content])
-        @endif
-    </figure>
+    @php
+        if (preg_match('/youtube.com\/((?:embed)|(?:watch))((?:\?v\=)|(?:\/))(\w+)/i', $attrs['src'], $matches)) {
+            $youtube_id = $matches[count($matches) - 1];
+        } elseif (preg_match('/youtu.be\/([a-zA-Z0-9_]+)\??/i', $attrs['src'], $matches)) {
+            $youtube_id = $matches[count($matches) - 1];
+        }
+    @endphp
+
+    @if ($youtube_id)
+        <figure class="{{ $class }}">
+            <iframe class="block aspect-video w-full" src="https://www.youtube.com/embed/{{ $youtube_id }}"
+                frameborder="0" allow="autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+            @if (isset($content))
+                @include('blocks.text', ['content' => $content])
+            @endif
+        </figure>
+    @endif
 @endif
